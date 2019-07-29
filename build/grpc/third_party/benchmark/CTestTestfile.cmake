b82193acc54dad390afccb4b251e04fbbc132cfc
chad <cdavi041@ucr.edu>
2019-07-28 22:02:36 -0700

    4

--- build/grpc/third_party/benchmark/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chad/6tesing_SCAMP/SCAMP/grpc/third_party/benchmark
-# Build directory: /home/chad/6tesing_SCAMP/SCAMP/build/grpc/third_party/benchmark
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
